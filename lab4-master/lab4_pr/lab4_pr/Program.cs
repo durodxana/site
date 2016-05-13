e2160856bc1683205a44887cba2cc2d34a452e53
Navruz Ergashev <durodxana@mail.ru>
2016-05-13 10:14:28 +0300

    Лабораторная работа №4

---
+++ lab4-master/lab4_pr/lab4_pr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(10,20);
+            Console.WriteLine("Perimetr: "+rectangle.getPerimetr()+" Square: "+rectangle.getSquare());
+
+
+            var circle = new Circle(10);
+            Console.WriteLine("Perimetr: " + circle.getPerimetr() + " Square: " + circle.getSquare());
+
+            var triangle = new Triangle(10, 20,20);
+            Console.WriteLine("Perimetr: " + triangle.getPerimetr() + " Square: " + triangle.getSquare());
+
+        }
+    }
+}
